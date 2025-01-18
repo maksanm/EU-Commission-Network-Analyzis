c900aebe6645890df67a729cdb8ea9668b02b4c7
Azmwet <azmwet@gmail.com>
2025-01-18 14:00:49 +0100

    basic graph done

---
+++ root_pointer.py
@@ -0,0 +1,3 @@
+from os import path
+
+ROOT = path.dirname(__file__)
---
+++ src/graphs/graph_builders/basic_graph.py
@@ -0,0 +1,100 @@
+import networkx as nx
+from os import listdir
+from os.path import isfile, join
+import json
+from itertools import combinations
+from root_pointer import ROOT
+MEETINGS_DIR = join(ROOT, "data/meetings")
+COMMISIONERS_DATA_DIR = join(ROOT, "data/members/commisioners_data_reviewed.json")
+MATCH_DATA_DIR = join(ROOT, "data/members/cabinent_members_match.json")
+UNREALTED_TITLES = [
+    "Director-General",
+    "Head of Task Force",
+    "Executive Vice-President",
+    "Acting Director-General",
+    "High Representative",
+    "Director",
+    "Secretary-General",
+    "Head of service",
+    "the Chair of the Regulatory Scrutiny Board",
+    "Acting Head of Service",
+    "Acting Head of service",
+    "Head of Service",
+    "Director of Office",
+    "Acting  Head of service"
+]
+
+def meetings_file_generator():
+    for file in listdir(MEETINGS_DIR):
+        file = join(MEETINGS_DIR, file)
+        if isfile(file):
+            yield file
+
+def create_graph_with_members() -> nx.Graph:
+    graph = nx.Graph()
+    with open(COMMISIONERS_DATA_DIR, "r", encoding="utf8") as f:
+        data = json.load(f)["commissioners"]
+
+    for commisioner in data:
+        name = commisioner["name"]
+        del commisioner["name"]
+        graph.add_node(name, **commisioner)
+    return graph
+
+def all_members_list():
+    with open(COMMISIONERS_DATA_DIR, "r", encoding="utf8") as f:
+        data = json.load(f)["commissioners"]
+    result = [elem["name"] for elem in data]
+    return result
+
+def members_match():
+    with open(MATCH_DATA_DIR, "r", encoding="utf8") as f:
+        return json.load(f)
+
+def extract_all_unique_members(file):
+    if "__unique_attendees.json" in file:
+        return None
+    with open(file, "r", encoding="utf8") as f:
+        data = json.load(f)["meetings"]
+    unique_attendees = set()
+    for meeting in data:
+        for attendee in meeting:
+            unique_attendees.add(attendee)
+
+    result = set()
+    members_list = all_members_list()
+    match = members_match()
+    for attendee in unique_attendees:
+        if attendee in members_list:
+            result.add(attendee)
+        else:
+            try:
+                result.add(match[attendee])
+            except KeyError:
+                if attendee.split("(")[1].replace(")", "") in UNREALTED_TITLES:
+                    pass
+                else:
+                    print(f"missing {attendee}")
+    return result
+
+def create_edges_for_meeting(file, graph: nx.Graph):
+    if "__unique_attendees.json" in file:
+        return None
+    attendees = extract_all_unique_members(file)
+    for combination in combinations(attendees, 2):
+        a1, a2 = combination
+        if graph.has_edge(a1, a2):
+            graph[a1][a2]["weight"] += 1
+        else:
+            graph.add_edge(a1, a2, weight=1)
+
+
+
+if __name__ == '__main__':
+    mean = 0
+    num = 0
+    graph = create_graph_with_members()
+    for file in meetings_file_generator():
+        create_edges_for_meeting(file, graph)
+    for name in graph.nodes:
+        print(graph.nodes[name])
